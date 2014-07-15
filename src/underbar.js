fe4d6b9da35a2089f7dd8f06ebd2c87a3ca6515e
Andrew Teich <hackreactor@Macintosh.local>
2014-07-15 19:54:22 -0400

    Implemented filter & reject

--- src/underbar.js
+++ src/underbar.js
@@ -83,16 +83,28 @@ var _ = {};
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var returnArray = new Array();
+    for (var i = 0; i < collection.length; i++) {
+      if(test(collection[i]))
+      {
+        returnArray.push(collection[i]);
+      }
+    }
+    return returnArray;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    return _.filter(collection, function(item){
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    
   };
 
 
